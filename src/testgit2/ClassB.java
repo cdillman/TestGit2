38fda172235c848adbe22cd231f449b7360bf67f
James G. Lombardo <jlombardo@wctc.edu>
2013-01-30 13:35:09 -0600

    changed my mind

---
+++ src/testgit2/ClassB.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit2;
+
+/**
+ *
+ * @author jlombardo
+ */
+public class ClassB {
+    
+}
